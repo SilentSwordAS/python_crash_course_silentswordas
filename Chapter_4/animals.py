94523e6fa007511d37bbaf9b9c37c3948f5f3ed9
SilentSwordAS <anuraag2002shukla@gmail.com>
2025-10-29 21:44:22 +0530

    Exercise 4-1 and 4-2

---
+++ Chapter_4/animals.py
@@ -0,0 +1,9 @@
+# Working on loops
+
+animals = ["Dog","Cat","Tortoise"]
+
+for animal in animals:
+    print(f"A {animal.lower()} would make a great pet.")
+
+print("Just GOATED pets")
+print("Any of these animals would make a great pet!")
---
+++ Chapter_4/pizzas.py
@@ -0,0 +1,8 @@
+# Iterating through a list using a loop
+
+fav_pizza = ["Pepperoni", "Margherita", "Farmhouse"]
+for pizza in fav_pizza:
+    print(f"I like {pizza} pizza.")
+
+print("I like Farmhouse very much.\nMargherita is a close second.\nI really love pizza!")
+
