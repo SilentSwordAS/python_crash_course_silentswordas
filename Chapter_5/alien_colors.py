3326abb54a354a2fda09edd4e6654cc011a89fa0
SilentSwordAS <anuraag2002shukla@gmail.com>
2026-01-05 02:39:03 +0530

    Exercise 5-3,5-4,5-5,5-6 and 5-7

---
+++ Chapter_5/alien_colors.py
@@ -0,0 +1,65 @@
+# Exercise 5-3 and 5-4
+
+# If condition is successful 
+alien_color = "green"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+
+# If condition is not successful
+alien_color = "red"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+
+# if-else chain(condition successful)
+
+alien_color="green"
+
+if alien_color=="green":
+    print("You earned 5 points!")
+else:
+    print("You earned 10 points!")
+
+# if-else chain(condition unsuccessful)
+
+alien_color="red"
+
+if alien_color=="green":
+    print("You earned 5 points!")
+else:
+    print("You earned 10 points!")
+
+# if-elif-else chain(first if condition successful)
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+elif alien_color == "yellow":
+    print("You earned 10 points!")
+elif alien_color == "red":
+    print("You earned 15 points!")
+
+# if-elif-else chain(first elif condition successful)
+
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+elif alien_color == "yellow":
+    print("You earned 10 points!")
+elif alien_color == "red":
+    print("You earned 15 points!")
+
+# if-elif-else chain(second elif condition successful)
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+elif alien_color == "yellow":
+    print("You earned 10 points!")
+elif alien_color == "red":
+    print("You earned 15 points!")
+
---
+++ Chapter_5/favourite_fruits.py
@@ -0,0 +1,14 @@
+favourite_fruits = ["bananas","apples","mangoes"]
+
+
+if "bananas" in favourite_fruits:
+    print("You really like bananas!")
+if "pomogrenates" in favourite_fruits:
+    print("You really like pomogrenates!")
+if "apples" in favourite_fruits:
+    print("You really like apples!")
+if "kiwis" in favourite_fruits:
+    print("You really like kiwis!")
+if "mangoes" in favourite_fruits:
+    print("You really like mangoes!")
+
